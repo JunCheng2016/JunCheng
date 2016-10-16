2ab62a918a41830c16e660a56c01cbc177f4cb40
BourbonZero <820425786@qq.com>
2016-10-16 15:11:01 +0800

    Strategy和User的dataserviceImpl

--- Server/data/StrategyDataServiceImpl.java
+++ Server/data/StrategyDataServiceImpl.java
@@ -7,18 +7,30 @@ import po.StrategyPO;
 public class StrategyDataServiceImpl {
 
 	public boolean insert(StrategyPO spo)throws RemoteException {
-		return false;
+		System.out.println("Insert succeed!");
+		return true;
 	}
 	
 	public boolean update(String name, StrategyPO po)throws RemoteException {
-		return false;
+		System.out.println("Update succeed!");
+		return true;
 	}
 
 	public boolean delete(StrategyPO spo)throws RemoteException {
-		return false;
+		if(spo.equals(null)){
+			System.out.println("Update failed!");
+		}else{
+			System.out.println("delete succeed!");
+		}
+		return true;
 	}
 
 	public StrategyPO find(String ID) throws RemoteException {
+		if(ID!=null){
+			System.out.println("Find succeed!");
+		}else{
+			System.out.println("Update failed!");
+		}
 		return null;
 	}
 }
--- Server/data/UserDataServiceImpl.java
+++ Server/data/UserDataServiceImpl.java
@@ -5,18 +5,26 @@ import po.UserPO;
 public class UserDataServiceImpl {
 
 	public UserPO find(String type,String ID) {
+		if(ID!=null){
+			System.out.println("Find succeed!");
+		}else{
+			System.out.println("Update failed!");
+		}
 		return null;
 	}
 
 	public boolean  update(String ID,UserPO po) {
-		return false;
+		System.out.println("Update succeed!");
+		return true;
 	}
 	
 	public boolean  insert(UserPO po) {
-		return false;
+		System.out.println("Insert succeed!");
+		return true;
 	}
 	
 	public boolean  check(UserPO po) {
-		return false;
+		System.out.println("Check succeed!");
+		return true;
 	}
 }
